a3a5dd931be9293f8db64ff1e4398fea4cec113a
Dawid Mucha <dawidmucha98@gmail.om>
2022-03-27 13:19:15 +0200

    LAB1

--- app/loan.php
+++ app/loan.php
@@ -1,66 +1,4 @@
 <?php
-<<<<<<< HEAD
-require_once dirname(__FILE__).'/../config.php';
-require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
-
-//pobranie parametrów
-function getParams(&$form) {
-	$form['amount'] = isset($_REQUEST['amount']) ? $_REQUEST['amount'] : null;
-	$form['payments'] = isset($_REQUEST['payments']) ? $_REQUEST['payments'] : null;
-	$form['upfront'] = isset($_REQUEST['upfront']) ? $_REQUEST['upfront'] : null;
-	$form['bankrate'] = isset($_REQUEST['bankrate']) ? $_REQUEST['bankrate'] : null;
-	$form['installment'] = isset($_REQUEST['installment']) ? $_REQUEST['installment'] : null;
-}
-
-// walidacja parametrów
-function validate(&$amount, &$payments, &$upfront, &$bankrate, &$messages) {	
-	if(!(isset($form['amount']) && isset($form['payments']) && isset($form['upfront']) && isset($form['bankrate']))) {
-		return false;
-	}
-
-	if($amount == null) {
-		$messages [] = "Nie podano wartości kredytu!";
-	}
-	
-	if(count($messages) != 0) return false;
-	else return true;
-}
-
-//wykonaj zadanie
-$installment = null;
-$infos = array();
-$messages = array();
-$hide_intro = false;
-
-getParams($form);
-if(validate($amount, $payments, $upfront, $bankrate, $messages)) {
-	$infos [] = 'Parametry poprawne. Wykonuję obliczenia';
-	
-	$form['amount'] = floatval($form['amount']);
-	$form['payments'] = floatval($form['payments']);
-	$form['upfront'] = floatval($form['upfront']);
-	$form['bankrate'] = floatval($form['bankrate']);
-
-	$form['installment'] = ($form['amount']-$form['upfront'])*(($form['bankrate']+100)/100)/$form['payments'];
-}
-
-$smarty = new Smarty();
-
-$smarty->assign('app_url',_APP_URL);
-$smarty->assign('root_path',_ROOT_PATH);
-$smarty->assign('page_title','Kalkulator kredytowy');
-$smarty->assign('page_description','Oblicz wysokość składki kredytu.');
-$smarty->assign('page_header','Kalkulator Kredytowy');
-
-$smarty->assign('hide_intro',$hide_intro);
-
-$smarty->assign('form',$form);
-$smarty->assign('installment',$installment);
-$smarty->assign('messages',$messages);
-$smarty->assign('infos',$infos);
-
-$smarty->display(_ROOT_PATH.'/app/loan.html');
-=======
 	require_once dirname(__FILE__).'/../config.php';
 
 	//fetch parameters
@@ -128,4 +66,3 @@ $smarty->display(_ROOT_PATH.'/app/loan.html');
 
 	include 'loan_view.php';
 ?>
->>>>>>> 317973d (LAB1)
--- app/loan_view.php
+++ app/loan_view.php
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<title>Kalkulator Kredytowy</title>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	</head>
-
-	<body>
-		<form action='<?php print(_APP_ROOT) ?>/app/loan.php' method='post'>
-			<div>
-				<label for='amount'>Wartość pożyczki:</label>
-				<input type='text' id='amount' name='amount' value='<?php out($amount) ?>' />
-			</div>
-
-			<div>
-				<label for='payments'>Podaj ilość rat:</label>
-				<input type='text' id='payments' name='payments' value='<?php out($payments) ?>' />
-			</div>
-
-			<div>
-				<label for='upfront'>Podaj wpłatę początkową:</label>
-				<input type='text' id='upfront' name='upfront' value='<?php out($upfront) ?>' />
-			</div>
-
-			<div>
-				<label for='bankrate'>Podaj wartość oprocentowania:</label>
-				<input type='text' id='bankrate' name='bankrate' value='<?php out($bankrate) ?>' />
-			</div>
-
-			<input type='submit' value='Oblicz' />
-		</form>
-
-		<?php 
-=======
 <?php
 	require_once dirname(__FILE__).'/../config.php';
 ?>
@@ -69,29 +33,12 @@
 
 		<?php
 			//wyświetlanie listy błędów
->>>>>>> 317973d (LAB1)
 			if(isset($messages)) {
 				if(count($messages) > 0) {
 					echo '<ol>';
 					foreach($messages as $key => $msg) {
 						echo '<li>'.$msg.'</li>';
 					}
-<<<<<<< HEAD
-					echo '.</ol>';
-				}
-			}
-		?>
-
-		<?php 
-			if(isset($installment)) { ?>
-				<div>
-					<?php echo 'Twoja rata wynosi '.$installment.'zł'; ?>
-				</div>
-		<?php	}
-		?>
-	</body>
-</html>
-=======
 					echo '</ol>';
 				}
 			}
@@ -106,4 +53,3 @@
 	</body>
 </html>
 
->>>>>>> 317973d (LAB1)
