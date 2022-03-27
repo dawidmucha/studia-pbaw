b6768e5ed0888a5fa77156ae2d9a3f5308609069
Dawid Mucha <dawidmucha98@gmail.om>
2022-03-27 13:35:20 +0200

    LAB1

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-<<<<<<< HEAD
-require_once dirname(__FILE__).'/config.php';
-
-//przekierowanie przeglądarki klienta (redirect)
-//header("Location: "._APP_URL."/app/loan.php");
-
-//przekazanie żądania do następnego dokumentu ("forward")
-include _ROOT_PATH.'/app/loan.php';
-=======
 	require_once dirname(__FILE__).'/config.php';
 	include _ROOT_PATH.'/app/loan.php';
 ?>
->>>>>>> 317973d (LAB1)
